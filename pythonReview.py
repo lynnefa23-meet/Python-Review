f13238be6f0956559e05291fb0c49c2b11ef8688
lynnefa23-meet <st.lynne.al-faqih@rosaryshs-j.com>
2022-07-31 11:34:10 +0300

    Update pythonReview.py

--- pythonReview.py
+++ pythonReview.py
@@ -16,9 +16,9 @@ def add_comment(youtube_video, username, comment_text):
 					youtube_video["Comments"][username] = comment_text
 					return youtube_video
 
-new_video = create_youtube_video("song", "good song")
+new_video = create_youtube_video("song", "amazing song")
 for i in range (495):
 	new_video= like(new_video)
 	new_video = dislike(new_video)
-	new_video = add_comment(new_video, "haha" ,"lola")
+	new_video = add_comment(new_video, "nice song" ,"oh yes!")
 print (new_video)
